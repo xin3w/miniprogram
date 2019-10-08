da01a40222b0781dcd398f87700a23ad425877af
zhanghui <xin3w@126.com>
2019-10-08 22:34:22 +0800

    模板应用
    1.如何定义模板（感觉站点内的所有模板都写在一个文件中更好，方便管理，模板统一样式定义）
    2.如何引用模板
    3.如何引用样式（当前页引用/全局引用）

--- pages/test/test2.js
+++ pages/test/test2.js
@@ -4,62 +4,83 @@ Page({
    * 页面的初始数据
    */
   data: {
-    msg:"test2"
+    msg: "test2",
+    dataTemplate: [{
+        id: 0,
+        src: "https://tuimeizi.cn/random?w=400&h=400&o=1&t=text",
+        title: "000"
+      },
+      {
+        id: 1,
+        src: "https://tuimeizi.cn/random?w=400&h=400&o=2&t=text",
+        title: "001"
+      },
+      {
+        id: 2,
+        src: "https://tuimeizi.cn/random?w=400&h=400&o=3&t=text",
+        title: "002"
+      },
+      {
+        id: 3,
+        src: "https://tuimeizi.cn/random?w=400&h=400&o=4&t=text",
+        title: "003"
+      }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     console.log("page test2 onload", options)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
     console.log("page test2 onReady")
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
     console.log("page test2 onShow")
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
     console.log("page test2 onHide")
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
